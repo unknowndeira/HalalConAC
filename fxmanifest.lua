04a45b18301035c8b02bc370f716d9c5ecdacb9a
Diamond <81833678+0x98a@users.noreply.github.com>
2022-02-17 22:45:32 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,29 @@
+fx_version 'adamant'
+
+game 'gta5'
+description 'Aba'
+version '1.5'
+
+
+client_scripts {
+    'Config.lua',
+    'Needs/client.lua',
+    --'Needs/injections.lua',
+    'Needs/Enumerators.lua'
+}
+
+server_scripts {
+    'Config.lua', 
+    'Needs/server.lua'
+}
+
+ui_page "Needs/html/index.html"
+file "Needs/html/index.html"
+
+
+
+
+
+
+
+
